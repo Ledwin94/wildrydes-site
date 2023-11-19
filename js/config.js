b807ca6e059db6e7d0b6ccc2f7216e34fb646cb0
Ledwin94 <104054206+Ledwin94@users.noreply.github.com>
2023-11-19 16:51:30 -0600

    changed for pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_u5BMeiCbF',         // e.g. us-east-2_u5BMeiCbF
-        userPoolClientId:   '16e91cv180sedi8kj8t72ompde',         // e.g. 16e91cv180sedi8kj8t72ompde
+        userPoolId:         'us-east-2_iHeKLKu3N',         // e.g. us-east-2_u5BMeiCbF
+        userPoolClientId:   'b337m6qs1kns6ti9s5hts07fu',         // e.g. 16e91cv180sedi8kj8t72ompde
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
